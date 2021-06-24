ec4ce3dde5ec6ab4cdc2d7e77800e78bccb8e6ea
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-23 21:34:44 -0700

    updated

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// initialize app config
+const app = express();
+connectDB();
+const port = process.env.PORT || 3000;
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+app.use("/api/conversations", require("./routes/api/conversations"));
+
+// listen
+app.listen(port, console.log("we are running on " + port));
