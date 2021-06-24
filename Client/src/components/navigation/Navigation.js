e492c9e34c4cf2baefb0cdcb419a1d9187b2e9e2
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-23 21:32:58 -0700

    updated styling

--- Client/src/components/navigation/Navigation.js
+++ Client/src/components/navigation/Navigation.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./navigation.css";
 
 const Navigation = () => {
-  return <div className="navigation">nav</div>;
+  return (
+    <div className="navigation">
+      <h1>Chatter</h1>
+    </div>
+  );
 };
 
 export default Navigation;
