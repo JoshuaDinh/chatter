c0c6e03d9c11641c99d1c31a9703e9f224c62fba
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-25 09:41:14 -0700

    created password salt hash with bcrypt & routeting to register user

--- Backend/routes/api/auth.js
+++ Backend/routes/api/auth.js
@@ -2,26 +2,38 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = require("../../models/User");
+var bcrypt = require("bcryptjs");
 
 // @route Post /api/register
 // @desc Register/create a new user account
 // @access Private
 
 router.post("/register", async (req, res) => {
-  try {
-    const user = await new User({
-      username: "josh",
-      email: "joshuadinh@gmail.com",
-      password: "123456",
-    });
+  const { name, email, password } = req.body;
 
-    // Check if username already exists
-    if ((user.username = user.username)) {
-      res.status(400).json("user exists");
+  try {
+    // See if user exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: "Email already exists" });
     }
 
+    //Create User instance
+    user = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Encrypt password
+    const salt = await bcrypt.genSaltSync(10);
+    // Hash specific users password
+    user.password = await bcrypt.hashSync(password, salt);
+
+    // Save new user to database
     await user.save();
-    res.send("ok");
+
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json("failed");
   }
--- Backend/routes/api/user.js
+++ Backend/routes/api/user.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
+var bcrypt = require("bcryptjs");
 
-// @route User /api/user
-// @desc
+// @route Post /api/register
+// @desc Register/create a new user account
 // @access Private
 
-router.get("/", (req, res) => {
-  res.status(200).json("user route");
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    // See if user exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    //Create User instance
+    user = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Encrypt password
+    const salt = await bcrypt.genSaltSync(10);
+    // Hash specific users password
+    user.password = await bcrypt.hashSync(password, salt);
+
+    // Save new user to database
+    await user.save();
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json("failed");
+  }
 });
 
 module.exports = router;
