03d06804335732289114523fb46ed0ac7906fae6
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-25 08:50:40 -0700

    updated

--- Backend/routes/api/auth.js
+++ Backend/routes/api/auth.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const User = require("../../models/User");
 
+// @route Post /api/register
+// @desc Register/create a new user account
+// @access Private
+
 router.post("/register", async (req, res) => {
   try {
     const user = await new User({
@@ -10,6 +14,7 @@ router.post("/register", async (req, res) => {
       email: "joshuadinh@gmail.com",
       password: "123456",
     });
+
     // Check if username already exists
     if ((user.username = user.username)) {
       res.status(400).json("user exists");
--- Client/src/components/message/Message.js
+++ Client/src/components/message/Message.js
@@ -6,11 +6,11 @@ const Message = ({ own }) => {
   return (
     <div className={`message ${own && "own"} `}>
       <div className="message-container ">
-        <img
+        {/* <img
           src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_624206636_200013332000928034_376810.jpg"
           alt=""
           className="message-avatar"
-        />
+        /> */}
         <p className="message-text">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores,
           <span className="message-timestamp own">
