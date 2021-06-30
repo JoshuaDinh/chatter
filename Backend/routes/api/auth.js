e934a95a65785c02f805bddfa3d777eb3e267276
Joshua Dinh <joshuahungdinh@gmail.com>
2021-06-30 06:18:24 -0700

    updated comments

--- Backend/routes/api/auth.js
+++ Backend/routes/api/auth.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
-const auth = require("../../middleware/token");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 // @route Post /api/auth
 // @desc Login user & get token
@@ -18,7 +19,7 @@ router.post("/", async (req, res) => {
       res
         .status(404)
         .json(
-          "Invalid Credentials, please  check that your username & password are correct"
+          "Invalid Credentials, please check that your username & password are correct"
         );
     }
 
@@ -37,13 +38,14 @@ router.post("/", async (req, res) => {
         id: user.id,
       },
     };
+
     jwt.sign(
       payload,
       config.get("jwtSecret"),
       { expiresIn: 3600 },
       (err, token) => {
         if (err) throw err;
-        res.json({ token });
+        res.status(200).json({ token });
       }
     );
 
