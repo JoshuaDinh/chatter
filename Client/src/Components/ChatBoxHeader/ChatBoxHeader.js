1ca3ac1ca08c93f776d3ff0d870c5d0589c48e0d
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-11 11:15:55 -0700

    added initial files

---
+++ Client/src/Components/ChatBoxHeader/ChatBoxHeader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./chatBoxHeader.css";
+
+const ChatBoxHeader = () => {
+  return (
+    <div className="chat-box-header">
+      <img
+        src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_624206636_200013332000928034_376810.jpg"
+        className="chat-box-header-avatar"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default ChatBoxHeader;
