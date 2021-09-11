51d604afb6e084f37d881039cd6125c88f137a24
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-11 11:58:13 -0700

    abilitity to add / connect with user and userid

--- Client/src/pages/messenger/Messenger.js
+++ Client/src/pages/messenger/Messenger.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./messenger.css";
 import { connect } from "react-redux";
 import Sidebar from "../../Components/Sidebar/Sidebar";
@@ -8,12 +8,15 @@ import Search from "../../Components/Search/Search";
 import ChatBox from "../../Components/ChatBox/ChatBox";
 import { io } from "socket.io-client";
 
-const Messenger = ({ toggleSearch }) => {
-  const [socket, setSocket] = useState(null);
+const Messenger = ({ toggleSearch, user }) => {
+  const socket = useRef(io("ws://localhost:8900"));
 
   useEffect(() => {
-    setSocket(io("ws://localhost:8900"));
-  }, []);
+    socket.current.emit("addUser", user?._id);
+    socket.current.on("getUsers", (socketUsers) => {
+      console.log(socketUsers);
+    });
+  }, [user]);
 
   return (
     <div className="messenger">
--- socket/socket.js
+++ socket/socket.js
@@ -4,6 +4,20 @@ const io = require("socket.io")(8900, {
   },
 });
 
+let socketUsers = [];
+
+const addUser = (userId, socketId) => {
+  !socketUsers.some((user) => user.userId === userId) &&
+    socketUsers.push({ userId, socketId });
+};
+
 io.on("connection", (socket) => {
   console.log("a user connected");
+  // io.to(socketId)("welcome", "hello this is socket");
+
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    // io.emit("getUsers", socketUsers);
+    console.log(socketUsers);
+  });
 });
