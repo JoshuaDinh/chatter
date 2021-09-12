4b003659b8bc118d4d021a896fa4765ce8e6275a
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-11 20:53:29 -0700

    created scroll into view functionality for messages

--- Client/src/pages/messenger/Messenger.js
+++ Client/src/pages/messenger/Messenger.js
@@ -6,22 +6,8 @@ import ChatMenu from "../../Components/ChatMenu/ChatMenu";
 import Personal from "../../Components/Personal/Personal";
 import Search from "../../Components/Search/Search";
 import ChatBox from "../../Components/ChatBox/ChatBox";
-import { io } from "socket.io-client";
 
 const Messenger = ({ toggleSearch, user }) => {
-  const socket = useRef();
-
-  useEffect(() => {
-    socket.current = io("ws://localhost:8900");
-  });
-
-  useEffect(() => {
-    socket.current.emit("addUser", user?._id);
-    socket.current.on("getUsers", (socketUsers) => {
-      console.log(socketUsers);
-    });
-  }, [user]);
-
   return (
     <div className="messenger">
       <Sidebar />
