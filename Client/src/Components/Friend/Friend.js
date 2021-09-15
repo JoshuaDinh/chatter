4e0c20d272451414844329f968ecdbb48500da84
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-15 16:09:10 -0700

    added friend component-

---
+++ Client/src/Components/Friend/Friend.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import "./friend.css";
+import axios from "axios";
+import { connect } from "react-redux";
+import { setCurrentChat } from "../../actions/currentChat";
+
+const Friend = ({ friend, chatId, setCurrentChat, authUser }) => {
+  const [friendData, setFriendData] = useState([]);
+  const [conversationId, setConversationId] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const friendInfo = await axios.get(`api/user/${friend}`);
+      setFriendData(friendInfo.data);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`api/conversations/${friend}`);
+      setConversationId(response.data[0]);
+    };
+    fetchData();
+  }, []);
+
+  const createNewChat = async () => {
+    const body = {
+      senderId: authUser._id,
+      recieverId: friendData._id,
+    };
+    if (conversationId) {
+      setCurrentChat(conversationId._id);
+    } else {
+      const response = await axios.post("api/conversations", body);
+      setCurrentChat(response.data._id);
+    }
+  };
+
+  return (
+    <div onClick={() => createNewChat()} className="friend">
+      <img
+        src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_624206636_200013332000928034_376810.jpg"
+        alt=""
+        className="friend-avatar"
+      />
+      <div className="friend-info">
+        <span>{friendData.username}</span>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    authUser: state.auth.user,
+  };
+};
+
+export default connect(mapStateToProps, {
+  setCurrentChat,
+})(Friend);
