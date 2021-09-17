9e6c630d0d63528fcbef0351e622fa0b028a1745
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-17 12:20:35 -0700

    added confirmatrion modal which needs suer to approve before deleted conversation thread

---
+++ Client/src/Components/ConfirmationModal/ConfirmationModal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./confirmationModal.css";
+import axios from "axios";
+import { connect } from "react-redux";
+import { setCurrentChat } from "../../actions/currentChat";
+
+const ConfirmationModal = ({ setModal, chatId, setCurrentChat }) => {
+  const deleteChat = async (event) => {
+    event.preventDefault();
+    await axios.post(`/api/conversations/${chatId}`);
+    setCurrentChat(null);
+    setModal(false);
+  };
+
+  return (
+    <div className="confirmationModal">
+      <h4>Are you sure you want to delete?</h4>
+      {chatId}
+      <div className="confirmationModal-button-container">
+        <button onClick={(event) => deleteChat(event)}>Delete</button>
+        <button onClick={() => setModal(false)}>Cancel</button>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { chatId: state.currentChat.chatId };
+};
+
+export default connect(mapStateToProps, {
+  setCurrentChat,
+})(ConfirmationModal);
