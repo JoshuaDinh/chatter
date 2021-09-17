61f720990884c9cbf9e7edb33f067c445f272ea2
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-17 14:59:30 -0700

    updated with functionality to retrieve friend information to display

--- Client/src/Components/ChatBoxHeader/ChatBoxHeader.js
+++ Client/src/Components/ChatBoxHeader/ChatBoxHeader.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./chatBoxHeader.css";
+import { connect } from "react-redux";
 import PhoneIcon from "@mui/icons-material/Phone";
 import VideoCallIcon from "@mui/icons-material/VideoCall";
 import CloseIcon from "@mui/icons-material/Close";
 import ConfirmationModal from "../ConfirmationModal/ConfirmationModal";
+import axios from "axios";
 
-const ChatBoxHeader = () => {
+const ChatBoxHeader = ({ selectedChatId, authUser }) => {
   const [modal, setModal] = useState(false);
+  const [friendId, setFriendId] = useState("");
+  const [friendData, setFriendData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Searchs for conversation in db - will return all members in conversation
+      const getConversation = await axios.get(
+        ` /api/conversations/single/${selectedChatId}`
+      );
+      // Get user._id of other person in conversation
+      const friend = getConversation.data?.members.find(
+        (friend) => friend !== authUser._id
+      );
+      // Get user information by id
+      const response = await axios.get(`api/user/${friend}`);
+      setFriendData(response.data);
+    };
+    fetchData();
+  }, [selectedChatId]);
+
   return (
     <div className="chat-box-header">
       <div className="chat-box-header-left">
@@ -16,7 +38,7 @@ const ChatBoxHeader = () => {
           alt=""
         />
         <div className="chat-box-header-info">
-          <h3>Joshua</h3>
+          <h3>{friendData.username}</h3>
           <span>Status: online</span>
         </div>
       </div>
@@ -33,4 +55,11 @@ const ChatBoxHeader = () => {
   );
 };
 
-export default ChatBoxHeader;
+const mapStateToProps = (state) => {
+  return {
+    selectedChatId: state.currentChat.chatId,
+    authUser: state.auth.user,
+  };
+};
+
+export default connect(mapStateToProps, {})(ChatBoxHeader);
